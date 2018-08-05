e6467ad83963770a914df6cd1897a517d7460faa
Alex Nudelman <nudelx@gmail.com>
2018-08-06 01:02:59 +0300

    added router and traffic

--- client/index.js
+++ client/index.js
@@ -1,14 +1,14 @@
 var CLIENT = {
   run: () => {
-    console.log("Content-type: text/html\n");
+    console.log("Content-type: text/html\n")
     const page = `<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"><meta name="theme-color" content="#000000">
     <link rel="manifest" href="/manifest.json">
     <link rel="shortcut icon" href="/favicon.ico"><title>React App</title>
-    <link href="static/css/main.0ab2866c.css" rel="stylesheet">
+    <link href="static/css/react_app.css" rel="stylesheet">
   </head><body><noscript>You need to enable JavaScript to run this app.</noscript>
   <div id="root"></div>
-    <script type="text/javascript" src="static/js/main.fad833fa.js"></script></body></html>`
-    console.log(page);
+    <script type="text/javascript" src="static/js/react_app.js"></script></body></html>`
+    console.log(page)
   }
 }
 
--- server/api.js
+++ server/api.js
@@ -1,13 +1,17 @@
 #!/usr/bin/node
 var API = {
-  createData: function (arr) {
+  router: {
+    1: function(creds, request, cheerio) {
+      return this.fetchDHCPClientsList(creds, request, cheerio)
+    },
+    2: function(creds, request, cheerio) {
+      return this.fetchTraffic(creds, request, cheerio)
+    }
+  },
+
+  createDataDhcp: function(arr) {
     const data = {}
     for (let i = 0; i + 4 < arr.length; i = i + 4) {
-      // console.log("NAME => ", arr[i])
-      // console.log("MAC => ", arr[i+1])
-      // console.log("IP => ", arr[i+2])
-      // console.log("TIME => ", arr[i+3])
-      // console.log('***********************')
       data[`ROW_${i / 4}`] = {
         NAME: arr[i],
         MAC: arr[i + 1],
@@ -18,7 +22,25 @@ var API = {
     return JSON.stringify(data)
   },
 
-  fetchDHCPClientsList: function (creds, request, cheerio) {
+  createDatatraffic: function(arr) {
+    const data = {}
+    for (let i = 0; i + 13 < arr.length; i = i + 13) {
+      data[`ROW_${i / 13}`] = {
+        IP: arr[i + 1],
+        MAC: arr[i + 2],
+        TOT_PKTS: arr[i + 3],
+        TOT_BYTE: arr[i + 4],
+        CUR_PKTS: arr[i + 5],
+        CUR_BYTE: arr[i + 6],
+        ICMP: arr[i + 7] + "/" + arr[i + 8],
+        UDP: arr[i + 9] + "/" + arr[i + 10],
+        SYT: arr[i + 11] + "/" + arr[i + 12]
+      }
+    }
+    return JSON.stringify(data)
+  },
+
+  fetchDHCPClientsList: function(creds, request, cheerio) {
     var self = this
     var url =
       "http://" +
@@ -34,8 +56,30 @@ var API = {
       }
       var $ = cheerio.load(html)
       var res = $("SCRIPT")
+      console.log(res)
       eval(res.html())
-      console.log(self.createData(DHCPDynList))
+      console.log(self.createDataDhcp(DHCPDynList))
+    })
+  },
+
+  fetchTraffic: function(creds, request, cheerio) {
+    var self = this
+    var url =
+      "http://" +
+      creds.username +
+      ":" +
+      creds.password +
+      "@" +
+      creds.host +
+      "/userRpm/SystemStatisticRpm.htm?interval=5&sortType=3&Num_per_page=50&Goto_page=1"
+    request(url, function(error, response, html) {
+      if (error) {
+        console.log("ooppsss error " + error + " " + response)
+      }
+      var $ = cheerio.load(html)
+      var res = $("script")
+      eval(res.html())
+      console.log(self.createDatatraffic(statList))
     })
   },
 
@@ -43,9 +87,13 @@ var API = {
     var request = require("request")
     var cheerio = require("cheerio")
     var creds = require("./cred.json")
+
     console.log("Content-Type: application/json; charset=utf-8\n")
-    this.fetchDHCPClientsList(creds, request, cheerio)
-  },
+    var routerKey = params.t || 1
+    this.router[routerKey]
+      ? this.router[routerKey].call(this, creds, request, cheerio)
+      : console.error("No such router")
+  }
 }
 
 module.exports = API
